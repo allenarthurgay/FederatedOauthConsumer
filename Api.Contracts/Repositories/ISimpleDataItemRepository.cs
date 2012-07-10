21b7cb2d516f45b0d2702793553c5a3c1ff1dc24
Allen Gay <allenarthurgay@gmail.com>
2012-07-10 13:42:34 -0700

    added abstract repo and service interface

---
+++ Api.Contracts/Repositories/ISimpleDataItemRepository.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using Data;
+
+namespace Api.Contracts.Repositories
+{
+    public interface ISimpleDataItemRepository<T> where T : IDataItem, new()
+    {
+        long Add(T dataitem);
+
+        void Edit(T dataitem);
+
+        void Delete(int id);
+
+        T GetById(int id);
+
+        T GetByUniqueId(Guid uniqueId);
+
+        void SetStatus(int id, Status status);
+
+        IEnumerable<T> GetValues(int page, int pagesize);
+
+        IEnumerable<T> GetValues(Status status, int page, int pagesize);
+    }
+    
+}
