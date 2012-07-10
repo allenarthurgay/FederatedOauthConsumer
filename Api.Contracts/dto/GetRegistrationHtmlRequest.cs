6dfff585f88f8290cf2879fdd03525d9760d904b
James Trinklein <jtrinklein@daptiv.com>
2012-07-10 14:47:40 -0700

    getRegistrationhtml stubbed out

---
+++ Api.Contracts/dto/GetRegistrationHtmlRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Api.Contracts.dto
+{
+	public class GetRegistrationHtmlRequest
+	{
+		public Guid PrincipalId { get; set; }
+		public string Service { get; set; }
+	}
+}
