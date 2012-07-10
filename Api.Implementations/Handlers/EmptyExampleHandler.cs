166ea4cbd60f163f3af471d824f1cf4648e937bc
Allen Gay <allenarthurgay@gmail.com>
2012-07-10 12:42:15 -0700

    pushing some initial fixes

---
+++ Api.Implementations/Handlers/EmptyExampleHandler.cs
@@ -0,0 +1,13 @@
+﻿using Api.Contracts;
+using ServiceStack.ServiceInterface;
+
+namespace Api.Implementations.Handlers
+{
+    public class EmptyExampleHandler: RestServiceBase<EmptyDto>
+    {
+        public override object OnGet(EmptyDto request)
+        {
+            return "OK";
+        }
+    }
+}
