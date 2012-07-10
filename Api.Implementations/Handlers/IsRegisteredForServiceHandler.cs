10f45765edb3b58c2571a3947055a796cb0c25d7
Brian <bdahl@daptiv.com>
2012-07-10 15:07:50 -0700

    Added Is registered for service handler info

---
+++ Api.Implementations/Handlers/IsRegisteredForServiceHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Api.Contracts.Services;
+using Api.Contracts.dto;
+using ServiceStack.ServiceInterface;
+
+namespace Api.Implementations.Handlers
+{
+	public class IsRegisteredForServiceHandler : RestServiceBase<IsRegisteredForServiceRequest>
+	{
+		private readonly IAuthConsumerService _service;
+
+		public IsRegisteredForServiceHandler(IAuthConsumerService service)
+		{
+			_service = service;
+		}
+
+		public override object OnGet(IsRegisteredForServiceRequest request)
+		{
+			return _service.IsRegisteredForService(request);
+		}
+	}
+}
