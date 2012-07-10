dec9672367a09a7ebd2c1071f961cc07555b5c7d
John Emau <jemau@daptiv.com>
2012-07-10 14:14:09 -0700

    Added UserTokenRecord Entity

---
+++ Data/DataItemBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ServiceStack.DataAnnotations;
+
+namespace Data
+{
+	public class DataItemBase : IDataItem
+	{
+		[AutoIncrement]
+		[PrimaryKey]
+		public int Id { get; set; }
+		public Guid UniqueId { get; set; }
+		public int Status { get; set; }
+		public DateTime Created { get; set; }
+		public DateTime Updated { get; set; }
+	}
+}
---
+++ Data/UserTokenRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data
+{
+	class UserTokenRecord : DataItemBase
+	{
+		public Guid UserId { get; set; }
+		
+		[StringLength(500)]
+		public string ServiceType { get; set; }
+		
+		[StringLength(500)]
+		public string Token { get; set; }
+	}
+}
