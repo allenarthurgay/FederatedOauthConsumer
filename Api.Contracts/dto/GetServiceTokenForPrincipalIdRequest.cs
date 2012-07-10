d7d3a108b3233ce296c5d9459f881f8ed3ded0f7
Brian <never.enough.hops@gmail.com>
2012-07-10 16:38:01 -0700

    added get service token for principal id

---
+++ Api.Contracts/dto/GetServiceTokenForPrincipalIdRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Api.Contracts.dto
+{
+	public class GetServiceTokenForPrincipalIdRequest
+	{
+		public Guid PrincipalId { get; set; }
+		public string Service { get; set; }
+	}
+}
---
+++ Api.Implementations/Handlers/GetServiceTokenForPrincipalIdHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Api.Contracts.Services;
+using Api.Contracts.dto;
+using ServiceStack.ServiceInterface;
+
+namespace Api.Implementations.Handlers
+{
+	public class GetServiceTokenForPrincipalIdHandler : RestServiceBase<GetServiceTokenForPrincipalIdRequest>
+	{
+		private readonly IAuthConsumerService _service;
+
+		public GetServiceTokenForPrincipalIdHandler(IAuthConsumerService service)
+		{
+			_service = service;
+		}
+
+		public override object OnGet(GetServiceTokenForPrincipalIdRequest request)
+		{
+			return _service.GetServiceTokenForPrincipalId(request);
+		}
+	}
+}
