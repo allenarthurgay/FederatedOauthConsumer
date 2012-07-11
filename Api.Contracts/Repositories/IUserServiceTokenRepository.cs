c718a9205d5e4914240063150e65daea39fb2b3a
Adam Kalman <akalman@daptive.com>
2012-07-11 11:16:26 -0700

    Readded entity for the token itself
    for multiple tokens per user

---
+++ Api.Contracts/Repositories/IUserServiceTokenRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Data;
+
+namespace Api.Contracts.Repositories
+{
+    public interface IUserServiceTokenRepository: ISimpleDataItemRepository<UserServiceTokenRecord>
+    {
+        UserServiceTokenRecord GetUserServiceTokenRecord(Guid Uid, Guid Sid);
+    }
+
+
+
+}
