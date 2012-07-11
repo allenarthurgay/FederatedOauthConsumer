5174dbf302dc65dd15975713e4ed21f1101ffb98
James Trinklein <jtrinklein@daptiv.com>
2012-07-11 16:47:42 -0700

    wired up supportedservices to database

--- Api.Contracts/Repositories/IServiceProviderRepository.cs
+++ Api.Contracts/Repositories/IServiceProviderRepository.cs
@@ -1,9 +1,11 @@
-﻿using Data;
+﻿using System.Collections.Generic;
+using Data;
 
 namespace Api.Contracts.Repositories
 {
     public interface IServiceProviderRepository: ISimpleDataItemRepository<ServiceProvider>
     {
         ServiceProvider GetByServiceName(string service);
+    	List<string> GetSupportedServices();
     }
 }
