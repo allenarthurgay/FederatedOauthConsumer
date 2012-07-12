0995196960ab5887d3c4271c716e9b16f955bfba
James Trinklein <jtrinklein@daptiv.com>
2012-07-11 17:08:35 -0700

    wired up get token, fixed exceptions on bad request to getregistrationhtml

--- Api.Implementations/Services/AuthConsumerService.cs
+++ Api.Implementations/Services/AuthConsumerService.cs
@@ -9,21 +9,21 @@ namespace Api.Implementations.Services
 {
 	public class AuthConsumerService : IAuthConsumerService
 	{
-	    private readonly IUserTokenRepository _userTokenRepository;
-	    private readonly IServiceProviderHtmlFactory _serviceProviderHtmlFactory;
-	    private readonly IServiceProviderRepository _serviceProviderRepository;
-	    private readonly IAuthTokenMassagerServiceFactory _authTokenMassagerServiceFactory;
+		private readonly IUserTokenRepository _userTokenRepository;
+		private readonly IServiceProviderHtmlFactory _serviceProviderHtmlFactory;
+		private readonly IServiceProviderRepository _serviceProviderRepository;
+		private readonly IAuthTokenMassagerServiceFactory _authTokenMassagerServiceFactory;
 
-	    public AuthConsumerService(IUserTokenRepository userTokenRepository,
-            IServiceProviderRepository serviceProviderRepository,
-            IServiceProviderHtmlFactory serviceProviderHtmlFactory,
-            IAuthTokenMassagerServiceFactory authTokenMassagerServiceFactory)
-        {
-            _userTokenRepository = userTokenRepository;
-	        _serviceProviderHtmlFactory = serviceProviderHtmlFactory;
-	        _serviceProviderRepository = serviceProviderRepository;
-	        _authTokenMassagerServiceFactory = authTokenMassagerServiceFactory;
-        }
+		public AuthConsumerService(IUserTokenRepository userTokenRepository,
+			IServiceProviderRepository serviceProviderRepository,
+			IServiceProviderHtmlFactory serviceProviderHtmlFactory,
+			IAuthTokenMassagerServiceFactory authTokenMassagerServiceFactory)
+		{
+			_userTokenRepository = userTokenRepository;
+			_serviceProviderHtmlFactory = serviceProviderHtmlFactory;
+			_serviceProviderRepository = serviceProviderRepository;
+			_authTokenMassagerServiceFactory = authTokenMassagerServiceFactory;
+		}
 
 		public GetSupportedServicesResponse GetSupportedServices(GetSupportedServicesRequest request)
 		{
@@ -35,67 +35,67 @@ namespace Api.Implementations.Services
 			if(!_serviceProviderRepository.GetSupportedServices().Contains(request.Service.ToLower()))
 			{
 				return new IsRegisteredForServiceResponse
-				       	{
-				       		IsRegistered =false
-				       	};
+						{
+							IsRegistered =false
+						};
 			}
 
 			var service = _serviceProviderRepository.GetByServiceName(request.Service);
 			var token = _userTokenRepository.GetUserTokenRecord(request.PrincipalId, service.Name);
-            			            
+									
 			return new IsRegisteredForServiceResponse {IsRegistered = token != null && !string.IsNullOrEmpty(token.Token)};
 		}
 
 		public GetRegistrationHtmlResponse GetRegistrationHtml(GetRegistrationHtmlRequest request)
 		{
-		    var service = _serviceProviderRepository.GetByServiceName(request.Service);
-		    var htmlProvider = _serviceProviderHtmlFactory.GetProviderHtmlService(service);
+			var service = _serviceProviderRepository.GetByServiceName(request.Service);
+			var htmlProvider = _serviceProviderHtmlFactory.GetProviderHtmlService(service);
 
-            if(null == htmlProvider)
-            {
-                throw  new Exception("Couldn't find an html provider for service named :" + request.Service);
-            }
-		    
-			return new GetRegistrationHtmlResponse
-			       	{
-						Html =htmlProvider.GetHtmlForService(service)
-					};
+			var html = string.Empty;
+			if(null != htmlProvider)
+			{
+				html = htmlProvider.GetHtmlForService(service);
+			}
+
+			return new GetRegistrationHtmlResponse {Html = html};
 		}
 
 		public GetServiceTokenForPrincipalIdResponse GetServiceTokenForPrincipalId(GetServiceTokenForPrincipalIdRequest request)
 		{
-			// todo actually go get the data
-			return new GetServiceTokenForPrincipalIdResponse {Token = "abc123"};
+			var service = _serviceProviderRepository.GetByServiceName(request.Service);
+			var token = _userTokenRepository.GetUserTokenRecord(request.PrincipalId, service.Name);
+
+			return new GetServiceTokenForPrincipalIdResponse { Token = token == null ? string.Empty : token.Token };
 
 		}
 
 		public void RegisterServiceToken(RegisterServiceTokenRequest request)
 		{
 			//does already exist
-            var service = _serviceProviderRepository.GetByServiceName(request.Service);
-		    var token = _userTokenRepository.GetUserTokenRecord(request.PrincipalId, service.Name);
-            var massager = _authTokenMassagerServiceFactory.GetMassagerForService(service);
+			var service = _serviceProviderRepository.GetByServiceName(request.Service);
+			var token = _userTokenRepository.GetUserTokenRecord(request.PrincipalId, service.Name);
+			var massager = _authTokenMassagerServiceFactory.GetMassagerForService(service);
 			var authToken = massager == null ? request.Token : massager.NormalizeToken(request.Token);
 
-            if(token ==  null)
-            {
-                _userTokenRepository.Add(new UserTokenRecord
-                                             {
+			if(token ==  null)
+			{
+				_userTokenRepository.Add(new UserTokenRecord
+											 {
 												 UniqueId = new Guid(),
-                                                 UserId = request.PrincipalId,
-                                                 ServiceType = request.Service,
-                                                 Token = authToken,
+												 UserId = request.PrincipalId,
+												 ServiceType = request.Service,
+												 Token = authToken,
 												 Created = DateTime.Now,
 												 Updated = DateTime.Now,
 												 Status = 1
-                                             });
-            }
-            else
-            {
-                token.Updated = DateTime.Now;
-                token.Token = authToken;
-                _userTokenRepository.Edit(token);
-            }
+											 });
+			}
+			else
+			{
+				token.Updated = DateTime.Now;
+				token.Token = authToken;
+				_userTokenRepository.Edit(token);
+			}
 		}
 	}
 }
